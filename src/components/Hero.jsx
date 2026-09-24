27f84d789f78adbd006d384887fb28f7819185eb
Ravi <ravivish968@gmail.com>
2026-09-25 00:05:43 +0530

    update style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -62,11 +62,11 @@ export default function Hero({ onResumeClick }) {
         <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
           <defs>
             <pattern id="smallGrid" width="40" height="40" patternUnits="userSpaceOnUse">
-              <path d="M 40 0 L 0 0 0 40" fill="none" stroke="rgba(145, 36, 255, 0.15)" strokeWidth="0.5"/>
+              <path d="M 40 0 L 0 0 0 40" fill="none" stroke="rgba(145, 36, 255, 0.35)" strokeWidth="0.5"/>
             </pattern>
             <pattern id="grid" width="160" height="160" patternUnits="userSpaceOnUse">
               <rect width="160" height="160" fill="url(#smallGrid)"/>
-              <path d="M 160 0 L 0 0 0 160" fill="none" stroke="rgba(145, 36, 255, 0.3)" strokeWidth="1"/>
+              <path d="M 160 0 L 0 0 0 160" fill="none" stroke="rgba(145, 36, 255, 0.5)" strokeWidth="1"/>
             </pattern>
           </defs>
           <rect width="100%" height="100%" fill="url(#grid)" />
